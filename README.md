989b223e0ab11956b6b0f86b22dd149a021b6487
Anirudh Kotamraju <akotamraju@berkeley.edu>
2025-04-28 17:10:48 -0700

    change filename

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 `pip install torch torchvision torchaudio # Dependencies for ultralytics/YOLO`
 
 # Example command
-if u have m1/2/3/4 mac chip use: `python yolo.py "videos/ween.mp4" --output_json "labels.json" --yolo_model "yolov8n.pt" --device "mps"`
+if u have m1/2/3/4 mac chip use: `python spacebar.py "videos/ween.mp4" --output_json "labels.json" --yolo_model "yolov8n.pt" --device "mps"`
 
-otherwise: `python yolo.py "videos/ween.mp4" --output_json "labels.json" --yolo_model "yolov8n.pt"`
+otherwise: `python spacebar.py "videos/ween.mp4" --output_json "labels.json" --yolo_model "yolov8n.pt"`
--- spacebar.py
+++ spacebar.py
@@ -1,6 +1,6 @@
 # Example command
-# if u have m1/2/3/4 mac chip use: python yolo_interactive.py "videos/ween.mp4" --output_json "labels.json" --yolo_model "yolov8n.pt" --device "mps"
-# otherwise: python yolo_interactive.py "videos/ween.mp4" --output_json "labels.json" --yolo_model "yolov8n.pt"
+# if u have m1/2/3/4 mac chip use: python spacebar.py "videos/ween.mp4" --output_json "labels.json" --yolo_model "yolov8n.pt" --device "mps"
+# otherwise: python spacebar.py "videos/ween.mp4" --output_json "labels.json" --yolo_model "yolov8n.pt"
 
 # !pip install ultralytics opencv-python pydub numpy scipy sounddevice # Ensure necessary libraries are installed
 # !pip install torch torchvision torchaudio # Dependencies for ultralytics/YOLO
